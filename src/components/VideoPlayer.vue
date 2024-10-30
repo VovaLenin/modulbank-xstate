ea89c8dc4734133495a9e99571a6f013a8a5cd5a
VovaLenin <ustinov2708@gmail.com>
2024-10-30 03:18:23 +0300

    микрофикс стилей и установка собственного источника видео

--- src/components/VideoPlayer.vue
+++ src/components/VideoPlayer.vue
@@ -78,8 +78,8 @@ export default defineComponent({
     const videoOptions: videojs.PlayerOptions = {
       sources: [
         {
-          // src: "/hls/playlist.m3u8",
-          src: "https://cdn.flowplayer.com/d9cd469f-14fc-4b7b-a7f6-ccbfa755dcb8/hls/383f752a-cbd1-4691-a73f-a4e583391b3d/playlist.m3u8",
+          src: "/hls/playlist.m3u8",
+          // src: "https://cdn.flowplayer.com/d9cd469f-14fc-4b7b-a7f6-ccbfa755dcb8/hls/383f752a-cbd1-4691-a73f-a4e583391b3d/playlist.m3u8",
           type: "application/x-mpegURL",
         },
       ],
